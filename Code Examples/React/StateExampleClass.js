be573487b28ef6ea7bce4955dcfebe106788049a
Ian Rios <ian.rios1a@gmail.com>
2019-12-11 10:42:42 -0500

    Update StateExampleClass.js

--- Code Examples/React/StateExampleClass.js
+++ Code Examples/React/StateExampleClass.js
@@ -1,6 +1,31 @@
 import React from 'react';
 
-class Example extends React.Component {
+class ExampleBind extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(){
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={this.handleClick}>
+          Click me
+        </button>
+      </div>
+    );
+  }
+}
+
+class ExampleInLine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,4 +45,4 @@ class Example extends React.Component {
   }
 }
 
-export default Example;
+export default {ExampleBind, ExampleInLine};
