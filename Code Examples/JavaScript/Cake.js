735a9b6f4320b1667f156c5641ad9555c52540c2
ianrios <ian.rios1a@gmail.com>
2020-03-13 14:11:24 -0400

    added cake.js

---
+++ Code Examples/JavaScript/Cake.js
@@ -0,0 +1,33 @@
+class Candle {
+	constructor(color, sparkler) {
+		this.color = color;
+		this.sparkler = sparkler;
+		this.candlesLit = false;
+	}
+	lightCandles(candleStatus) {
+		this.candlesLit = candleStatus
+	}
+}
+
+class Cake {
+	constructor(type, layers, icing, candleNum) {
+		this.type = type;
+		this.layers = layers;
+		this.icing = icing;
+		this.candles = []
+		this.candleNum = candleNum
+	}
+	generateCandles() {
+		for (let index = 0; index < this.candleNum; index++) {
+			this.candles.push(new Candle("blue", false))
+			console.log(this.candles)
+		}
+	}
+}
+
+
+var birthdayCake = new Cake("birthday", 3, true, 24);
+console.log("before generate", birthdayCake)
+birthdayCake.generateCandles()
+console.log("after generate", birthdayCake)
+//
